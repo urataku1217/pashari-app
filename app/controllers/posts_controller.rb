79087b5278ddf59ecd375d4500d7ec0b3a367dfc
urataku1217 <urataku.com@gmail.com>
2020-11-16 18:07:17 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:name, :image, :text, :outer)
+    params.require(:post).permit(:name, :image, :text, :outer, :base, :inner, :accessory)
   end
 
 end
